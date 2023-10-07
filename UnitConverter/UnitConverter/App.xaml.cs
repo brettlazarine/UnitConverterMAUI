2bca8c02516115a7c8529f2a4273c7322dbd2ead
Brett Lazarine <jblazarine@gmail.com>
2023-10-07 02:10:42 -0500

    complete project

--- UnitConverter/UnitConverter/App.xaml.cs
+++ UnitConverter/UnitConverter/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace UnitConverter;
+﻿using UnitConverter.MVVM.Views;
+
+namespace UnitConverter;
 
 public partial class App : Application
 {
@@ -6,6 +8,6 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		MainPage = new NavigationPage(new MenuView());
 	}
 }
